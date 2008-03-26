88e3d8e1bb76a2b00b7211ce7b16213e62864e29
bhagwat.vivek <bhagwat.vivek@ac3fcf6a-5c49-0410-827d-79ed2f0d3f91>
2008-03-26 21:02:15 +0000

    implemented TrafficActor and TrafficLocation

--- trunk/gridworldtraffic/TrafficActor.java
+++ trunk/gridworldtraffic/TrafficActor.java
@@ -5,10 +5,32 @@
 
 package gridworldtraffic;
 
+import info.gridworld.actor.*;
+import info.gridworld.grid.*;
+import info.gridworld.gui.*;
+import info.gridworld.world.*;
+import java.awt.Color;
+
 /**
  *
- * @author vivek
+ * @author vivek bhagwat and alan joyce and kevin lo
  */
-public class TrafficActor {
+public abstract class TrafficActor extends Actor 
+{
+    private Grid<TrafficActor> grid;
+    private Location location;
+    private int direction;
+    private Color color;
+    private int speed; //squares per step
 
+    public int getSpeed() 
+    {
+        return speed;
+    }
+
+    public void setSpeed(int speed) 
+    {
+        this.speed = speed;
+    }    
+    
 }
