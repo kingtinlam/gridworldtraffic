d30651f266a867e62d0eed2593ee97836b0e077d
bhagwat.vivek <bhagwat.vivek@ac3fcf6a-5c49-0410-827d-79ed2f0d3f91>
2008-03-28 21:04:58 +0000

    Implemented more Vehicle functions, pseudocoded act method.

--- gridworldtraffic/src/gridworldtraffic/Vehicle.java
+++ gridworldtraffic/src/gridworldtraffic/Vehicle.java
@@ -56,20 +56,57 @@ public class Vehicle extends Actor
         this.speed = speed;
     }
     
+    public Actor getInFront()
+    {
+        return getGrid().get(getLocation().getAdjacentLocation(0));
+    }
+    
+    //only true if location in front of it is a traffic light
     public boolean atIntersection()
     {
         Location locInFront = getLocation().getAdjacentLocation(0);
-        if(getGrid().get(locInFront) instanceof TrafficLight)
+        Actor front = getGrid().get(locInFront);
+        if(front instanceof TrafficLight)
         {
-            return true; //to be reimplemented
+            return true;            
         }
         return false;
     }
     
+    //first check road
+    //then check in front of you
+    public int speedLimit()
+    {
+        int limit = 0;
+        TrafficLocation loc = (TrafficLocation)(getLocation());
+        limit = loc.getSpeedLimit();
+        
+        try
+        {
+            Vehicle front = (Vehicle)getInFront();
+            if(front.getSpeed() < limit)
+                limit = front.getSpeed();
+        }
+        catch(ClassCastException ex)
+        {
+            System.out.println("No vehicle in front, " + ex.getLocalizedMessage());
+        }
+        
+        
+        return limit;
+    }
     
+    private void stop()
+    {
+        setSpeed(0);
+    }
     
     public void act()
     {
+        //if at intersection, wait until light turns green
+        //check speed limit, go at that speed.
+        //if want to turn at intersection, do so.
+        //if want to change lanes, do so.
         
     }
 }
